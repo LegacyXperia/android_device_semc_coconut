cb0247119d613a49deac18c727918982679e05e8
freexperia <jerpelea@gmail.com>
2012-02-25 12:22:09 +0200

    semc: moved usb DevID / device

--- device_coconut.mk
+++ device_coconut.mk
@@ -28,6 +28,7 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/coconut/overlay
 
 # Init files
 PRODUCT_COPY_FILES += \
+    device/semc/coconut/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/coconut/prebuilt/bootrec:root/sbin/bootrec \
     device/semc/coconut/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/coconut/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
