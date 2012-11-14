d9f527a1cd9415fe6335cfbaba1857128d34ff8b
codeworkx <codeworkx@cyanogenmod.com>
2012-11-14 15:45:49 +0100

    coconut: device specific part for the new two-stage boot method
    Ported from http://review.cyanogenmod.org/#/c/26440/
    Author: codeworkx <codeworkx@cyanogenmod.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I2dc9299571ce71409c4b9a86b19a6feb5cfcdc25

--- coconut.mk
+++ coconut.mk
@@ -20,9 +20,11 @@ PRODUCT_COPY_FILES += \
     device/semc/coconut/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/coconut/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/coconut/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/coconut/prebuilt/bootrec-device:root/sbin/bootrec-device \
-    device/semc/coconut/recovery.fstab:root/recovery.fstab
+    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle
+
+# Device specific part for two-stage boot
+PRODUCT_COPY_FILES += \
+    device/semc/coconut/recovery/bootrec-device:recovery/bootrec-device
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
