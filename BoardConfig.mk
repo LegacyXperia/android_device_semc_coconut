4005114f2f4fe25c3706e5ef40a2adc7618b9546
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 03:38:51 +0200

    coconut: some cleanup
    * Remove Phone overlay (mdpi fix is merged in Phone app)
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I76e48f85cb1a6ca1270bba3477f3a8f601b1e54a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/coconut/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_coconut_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/coconut/recovery/init.rc
 
-#Sensors
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA250_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
@@ -16,6 +18,6 @@ BOARD_USES_STEREO_HW_SPEAKER := true
 
 TARGET_OTA_ASSERT_DEVICE := WT19a,WT19i,coconut
 
-#Bluetooth
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/coconut/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/semc/coconut/bluetooth/vnd_coconut.txt
--- cm.mk
+++ cm.mk
@@ -1,19 +1,19 @@
 # Inherit device configuration
 $(call inherit-product, device/semc/coconut/full_coconut.mk)
 
-## Specify phone tech before including full_phone
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-#Set build fingerprint / ID / Product Name ect.
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=WT19i BUILD_FINGERPRINT="SEMC/WT19i_1254-2184/WT19i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_coconut
 PRODUCT_DEVICE := coconut
