46401ea49c501d5512c7e6185780f69f32cdb2d6
Michael Bestas <mikeioannina@gmail.com>
2013-04-24 17:09:56 +0300

    coconut: rename logo for nAa 3.0.x
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I6ae1cb4931cccef8083c9ec277320105172328df

--- coconut.mk
+++ coconut.mk
@@ -18,7 +18,7 @@ PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_COPY_FILES += \
     device/semc/coconut/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/coconut/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle
+    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/initlogo.rle
 
 # Device specific part for two-stage boot
 PRODUCT_COPY_FILES += \
