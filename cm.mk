30b2014026a5437c0124909473e4dd700d9c5005
Michael Bestas <mikeioannina@gmail.com>
2013-09-17 21:55:49 +0300

    Update live wallpapers
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I123ac20fb009f9906045aa363d97ade8683aa478

--- cm.mk
+++ cm.mk
@@ -23,14 +23,15 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
-    HoloSpiralWallpaper \
-    MagicSmokeWallpapers \
-    NoiseField \
     Galaxy4 \
+    HoloSpiralWallpaper \
     LiveWallpapers \
     LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
     VisualizationWallpapers \
-    PhaseBeam
+    PhotoTable
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=WT19i BUILD_FINGERPRINT="SEMC/WT19i_1254-2184/WT19i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
